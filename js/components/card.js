ed4c3c6a31e717669287018c116851e8d392e064
RodrigoLago <rodrigazo98@gmail.com>
2023-05-01 13:30:29 -0300

    Modifico forma de mostrar moneda

--- js/components/card.js
+++ js/components/card.js
@@ -3,7 +3,7 @@ export const Card = (id, nombre, precio, image, clase, favChecked, currency) => 
         <img src = ${image}>
         <div class="texto">
             <label class="titleCard"> ${nombre} </label>
-            <label class="precioCard"> ${currency}$${precio} </label>
+            <label class="precioCard"> ${precio}&nbsp${currency}</label>
         </div>
         <div > 
             <input id="${id}-fav" class="heart" data-id = ${id} type="checkbox" ${favChecked}/>
